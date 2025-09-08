27394510168278d6045adc9945bbd63b84e83dd0
kaviarasur4 <kaviarasur3@gmail.com>
2025-09-08 13:48:05 +0530

    changes

--- src/shared/components/About.jsx
+++ src/shared/components/About.jsx
@@ -15,6 +15,7 @@ export default function About() {
                                 <p className=''>breakfirst</p>
                                 <p className=''>dinner</p>
                             </div>
+                            
                         </div>
                     </div>
                 </div>
